9bd0863d84cdb3e20d55ac245058ae079bfd5cb8
S.Maha Lakshmi <mahijustin116@gmail.com>
2025-12-05 20:14:16 +0530

    Update Navbar title to include 'S'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <a href="#home" className="text-2xl font-bold text-gradient">
-            Mahalakshmi
+            Mahalakshmi S
           </a>
 
           {/* Desktop Navigation */}
